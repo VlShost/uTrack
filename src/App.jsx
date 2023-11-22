80ec9fb332bf2a4c8d2dff588a07e6a575f15fba
VlShost <vlshost9386@gmail.com>
2023-11-22 15:34:48 +0200

    header markup and style utils

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import './App.css';
-
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/SharedLayout/Layout';
 import Home from './pages/Home';
+import Details from './pages/Details';
+import Faq from './pages/Faq';
+
+import './App.css';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="details" element={<Details />} />
+        <Route path="faq" element={<Faq />} />
         <Route path="*" element={<Navigate to={'/'} />} />
       </Route>
     </Routes>
--- src/components/SharedLayout/Header/Header.jsx
+++ src/components/SharedLayout/Header/Header.jsx
@@ -1,5 +1,33 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <nav>
+        <a href="/">Logo</a>
+        <ul>
+          <li>
+            <a href="">Про нас</a>
+          </li>
+          <li>
+            <a href="">Переваги</a>
+          </li>
+          <li>
+            <a href="">Асортимент</a>
+          </li>
+          <li>
+            <a href="">Партнери</a>
+          </li>
+          <li>
+            <a href="">Мапа</a>
+          </li>
+          <li>
+            <a href="">FAQ</a>
+          </li>
+        </ul>
+      </nav>
+      <a href="tel:+38 050 345 14 15">+38 050 345 14 15</a>
+      <a href="/">m-auth</a>
+    </header>
+  );
 };
 
 export default Header;
