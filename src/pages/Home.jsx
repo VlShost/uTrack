d0fc9d257d2c280de30a55f97415e11eb6b20605
VlShost <vlshost9386@gmail.com>
2023-11-23 13:26:35 +0200

    add home page sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import Hero from '../components/Hero';
+import About from '../components/About';
+import Benefits from '../components/Benefits';
+import Stock from '../components/Stock';
+import Reviews from '../components/Reviews';
+import Location from '../components/Location';
 
 const Home = () => {
   return (
-    <section>
+    <>
       <Hero />
-    </section>
+      <About />
+      <Benefits />
+      <Stock />
+      <Reviews />
+      <Location />
+    </>
   );
 };
 
