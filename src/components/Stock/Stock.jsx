c3fd2b94f053f44e6399ad44b2afe6546aeab04b
Anatoliy Matsakov <mactolik1993@gmail.com>
2024-01-22 22:28:15 +0200

    fix imgs and add media

--- src/components/Stock/Stock.jsx
+++ src/components/Stock/Stock.jsx
@@ -3,6 +3,14 @@ import { Autoplay, Pagination, Keyboard } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
+import polls_1x from '../../assets/images/stock/polls/sensor-1x.png';
+import polls_2x from '../../assets/images/stock/polls/sensor-2x.png';
+
+import tracker_1x from '../../assets/images/stock/trackers/uTrackR3-1x.png';
+import tracker_2x from '../../assets/images/stock/trackers/uTrackR3-2x.png';
+
+import validator_1x from '../../assets/images/stock/validators/mpi-102-1x.png';
+import validator_2x from '../../assets/images/stock/validators/mpi-102-2x.png';
 
 import css from './Stock.module.css';
 
@@ -12,7 +20,6 @@ const Stock = () => {
       <div className={css.container}>
         <h2 className={css.title}>Що ми пропонуємо?</h2>
         <Swiper
-          spaceBetween={40}
           grabCursor={true}
           autoplay={{
             delay: 5000,
@@ -27,14 +34,17 @@ const Stock = () => {
             375: {
               slidesPerView: 1,
               loop: true,
+              spaceBetween: 24,
             },
             664: {
               slidesPerView: 2,
               loop: true,
+              spaceBetween: 24,
             },
             1440: {
               slidesPerView: 3,
               loop: false,
+              spaceBetween: 41,
             },
           }}
           keyboard={{ enabled: true }}
@@ -44,65 +54,13 @@ const Stock = () => {
             <div className={css.wrapper_card}>
               <div className={css.card}>
                 <div className={css.img}>
-                  <picture>
-                    <source
-                      media="(min-width: 1440px)"
-                      srcSet="../../../src/assets/images/stock/trackers/uTrackR3-1x.png 1x, ../../../src/assets/images/stock/trackers/uTrackR3-2x.png 2x"
-                      type="image/png"
-                    />
-                    <source
-                      media="(min-width: 744px)"
-                      srcSet="../../../src/assets/images/stock/trackers/uTrackR3-1x.png 1x, ../../../src/assets/images/stock/trackers/uTrackR3-2x.png 2x"
-                      type="image/png"
-                    />
-                    <source
-                      media="(min-width: 375px)"
-                      srcSet="../../../src/assets/images/stock/trackers/uTrackR3-1x.png 1x, ../../../src/assets/images/stock/trackers/uTrackR3-2x.png 2x"
-                      type="image/png"
-                    />
-                    <img
-                      src="../../../src/assets/images/stock/trackers/uTrackR3-1x.png"
-                      alt="tracker"
-                    />
-                  </picture>
-                </div>
-
-                <div className={css.infoWrapper}>
-                  <p className={css.name}>GPS-трекери для всіх видів транспорту</p>
-                  <p className={css.info}>
-                    Живлення від прикурювача або в умовах стаціонарного підключення до автомобіля
-                  </p>
-                </div>
-
-                <button className={css.btn}>Дізнатися більше</button>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={css.wrapper_card}>
-              <div className={css.card}>
-                <div className={css.img}>
-                  <picture>
-                    <source
-                      media="(min-width: 1440px)"
-                      srcSet="../../../src/assets/images/stock/validators/mpi-102-1x.png 1x, ../../../src/assets/images/stock/validators/mpi-102-2x.png 2x"
-                      type="image/png"
-                    />
-                    <source
-                      media="(min-width: 744px)"
-                      srcSet="../../../src/assets/images/stock/validators/mpi-102-1x.png 1x, ../../../src/assets/images/stock/validators/mpi-102-2x.png 2x"
-                      type="image/png"
-                    />
-                    <source
-                      media="(min-width: 375px)"
-                      srcSet="../../../src/assets/images/stock/validators/mpi-102-1x.png 1x 1x, ../../../src/assets/images/stock/validators/mpi-102-2x.png 2x"
-                      type="image/png"
-                    />
-                    <img
-                      src="../../../src/assets/images/stock/validators/mpi-102-1x.png 1x"
-                      alt="tracker"
-                    />
-                  </picture>
+                  <img
+                    loading="lazy"
+                    className={css.img}
+                    srcSet={`${tracker_1x} 1x, ${tracker_2x} 2x`}
+                    src={tracker_1x}
+                    alt="Трекер"
+                  />
                 </div>
 
                 <div className={css.infoWrapper}>
@@ -120,24 +78,37 @@ const Stock = () => {
             <div className={css.wrapper_card}>
               <div className={css.card}>
                 <div className={css.img}>
-                  <picture>
-                    <source
-                      media="(min-width: 1440px)"
-                      srcSet="../../../src/assets/images/stock/polls/sensor-1x.png 1x, ../../../src/assets/images/stock/polls/sensor-2x.png 2x"
-                      type="image/png"
-                    />
-                    <source
-                      media="(min-width: 744px)"
-                      srcSet="../../../src/assets/images/stock/polls/sensor-1x.png 1x, ../../../src/assets/images/stock/polls/sensor-2x.png 2x"
-                      type="image/png"
-                    />
-                    <source
-                      media="(min-width: 375px)"
-                      srcSet="../../../src/assets/images/stock/polls/sensor-1x.png 1x, ../../../src/assets/images/stock/polls/sensor-2x.png 2x"
-                      type="image/png"
-                    />
-                    <img src="../../../src/assets/images/stock/polls/sensor-1x.png" alt="tracker" />
-                  </picture>
+                  <img
+                    loading="lazy"
+                    className={css.img}
+                    srcSet={`${validator_1x} 1x, ${validator_2x} 2x`}
+                    src={validator_1x}
+                    alt="Валідатор"
+                  />
+                </div>
+
+                <div className={css.infoWrapper}>
+                  <p className={css.name}>GPS-трекери для всіх видів транспорту</p>
+                  <p className={css.info}>
+                    Живлення від прикурювача або в умовах стаціонарного підключення до автомобіля
+                  </p>
+                </div>
+
+                <button className={css.btn}>Дізнатися більше</button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={css.wrapper_card}>
+              <div className={css.card}>
+                <div className={css.img}>
+                  <img
+                    loading="lazy"
+                    className={css.img}
+                    srcSet={`${polls_1x} 1x, ${polls_2x} 2x`}
+                    src={polls_1x}
+                    alt="Датчик рівня палива"
+                  />
                 </div>
 
                 <div className={css.infoWrapper}>
