761b5e499569d86e26c3fbda4c750e7d98feae09
VlShost <vlshost9386@gmail.com>
2024-02-23 14:51:21 +0200

    add favicon and modal portal

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,38 @@
+import { createPortal } from 'react-dom';
+import { useEffect } from 'react';
+import styles from './Modal.module.css';
+
+const modalRoot = document.querySelector('#modal-root');
+
+const Modal = ({ children, toggleModal }) => {
+  const handleBackdropClick = (e) => {
+    if (e.currentTarget === e.target) {
+      toggleModal();
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === 'Escape') {
+        toggleModal();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [toggleModal]);
+
+  return createPortal(
+    <div className={styles.modalBackdrop} onClick={handleBackdropClick}>
+      <div className={styles.modalContent}>
+        <div>{children}</div>
+      </div>
+    </div>,
+    modalRoot
+  );
+};
+
+export default Modal;
