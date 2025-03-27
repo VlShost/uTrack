58e38bc67b55981a5592126024413eeca9b25936
VlShost <vlshost9386@gmail.com>
2025-03-27 13:53:30 +0200

    add alias for absolute path to data folder

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), svgr()],
@@ -8,4 +13,9 @@ export default defineConfig({
     port: 3000,
   },
   base: '/vlad/',
+  resolve: {
+    alias: {
+      '@data': path.resolve(__dirname, 'src/data'),
+    },
+  },
 });
