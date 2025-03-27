b1d7aff27bee25d8ca68ea3cc6850613a8f188fd
VlShost <vlshost9386@gmail.com>
2025-03-27 13:57:59 +0200

    fixed images download bug

--- src/components/Stock/Stock.jsx
+++ src/components/Stock/Stock.jsx
@@ -7,91 +7,94 @@ import 'swiper/scss/pagination';
 import 'swiper/scss/keyboard';
 import 'swiper/scss/virtual';
 
+import data from '@data/stock.json';
 import StockCardItem from '../StockCardItem';
 
-import { getStock } from '../../services/fetchData';
-
 import css from './Stock.module.scss';
+
+const images = import.meta.glob('/src/assets/images/**/*.{png,jpg,jpeg,webp}', { eager: true });
 
 const Stock = () => {
   const [stock, setStock] = useState([]);
 
   useEffect(() => {
-    getStock().then((data) => {
-      setStock(data.results);
-    });
-  }, []);
+    const processedStock = data.map((item) => {
+      // Приводим путь JSON к такому же формату, как в import.meta.glob()
+      const x1Path = `/src/assets/${item.images.x1}`;
+      const x2Path = `/src/assets/${item.images.x2}`;
 
-  console.log('stock:', stock);
+      return {
+        ...item,
+        images: {
+          x1: images[x1Path]?.default || '',
+          x2: images[x2Path]?.default || '',
+          alt: item.images.alt,
+        },
+      };
+    });
+
+    setStock(processedStock);
+  }, []);
 
   return (
     <section id="stock" className={css.section}>
       <div className={css.container}>
         <h2 className={css.title}>Що ми пропонуємо?</h2>
 
-        <Swiper
-          modules={[Pagination, Keyboard, Autoplay]}
-          className={css.swiper}
-          grabCursor={true}
-          keyboard={{ enabled: true }}
-          centeredSlides={true}
-          watchOverflow={true}
-          slidesPerView={1}
-          spaceBetween={34}
-          autoplay={{
-            delay: 5000,
-            disableOnInteraction: false,
-            pauseOnMouseEnter: true,
-          }}
-          speed={1500}
-          pagination={{
-            el: '#swiperStockPagination',
-            clickable: true,
-          }}
-          breakpoints={{
-            744: {
-              slidesPerView: 2,
-              centeredSlides: false,
-              loop: true,
-              spaceBetween: 10,
-            },
-            1024: {
-              slidesPerView: 3,
-              centeredSlides: false,
-              loop: false,
-              spaceBetween: 10,
-            },
-            1440: {
-              slidesPerView: 3,
-              centeredSlides: false,
-              spaceBetween: 24,
-            },
-          }}
-        >
-          {!stock ||
-            (stock.length === 0 && (
-              <MagnifyingGlass
-                ariaLabel="magnifying-glass-loading"
-                glassColor="#c0efff"
-                color="#0D6197"
-                wrapperClass={css.spinner}
-              />
-            ))}
-          {stock ? (
-            stock.map((card, i) => (
+        {stock.length === 0 ? (
+          <MagnifyingGlass
+            ariaLabel="magnifying-glass-loading"
+            glassColor="#c0efff"
+            color="#0D6197"
+            wrapperClass={css.spinner}
+          />
+        ) : (
+          <Swiper
+            modules={[Pagination, Keyboard, Autoplay]}
+            className={css.swiper}
+            grabCursor={true}
+            keyboard={{ enabled: true }}
+            centeredSlides={true}
+            watchOverflow={true}
+            slidesPerView={1}
+            spaceBetween={34}
+            autoplay={{
+              delay: 5000,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+            speed={1500}
+            pagination={{
+              el: '#swiperStockPagination',
+              clickable: true,
+            }}
+            breakpoints={{
+              744: {
+                slidesPerView: 2,
+                centeredSlides: false,
+                loop: true,
+                spaceBetween: 10,
+              },
+              1024: {
+                slidesPerView: 3,
+                centeredSlides: false,
+                loop: false,
+                spaceBetween: 10,
+              },
+              1440: {
+                slidesPerView: 3,
+                centeredSlides: false,
+                spaceBetween: 24,
+              },
+            }}
+          >
+            {stock.map((card, i) => (
               <SwiperSlide key={i} className={css.slide}>
                 <StockCardItem data={card} />
               </SwiperSlide>
-            ))
-          ) : (
-            <MagnifyingGlass
-              ariaLabel="magnifying-glass-loading"
-              glassColor="#c0efff"
-              color="#0D6197"
-              wrapperClass={css.spinner}
-            />
-          )}
-        </Swiper>
+            ))}
+          </Swiper>
+        )}
 
         <div className={css.swiperPagination} id="swiperStockPagination"></div>
       </div>
--- src/components/StockCardItem/StockCardItem.jsx
+++ src/components/StockCardItem/StockCardItem.jsx
@@ -13,22 +13,22 @@ const StockCardItem = ({ data }) => {
     setIsModalOpen((prev) => !prev);
   };
 
-  const { images, name, info, details } = data;
+  const { images = {}, name = 'Без назви', info = 'Опис відсутній', details = {} } = data;
 
   return (
     <div className={css.card}>
       <div className={css.imgWrapper}>
-        {images.x1 !== '' || images.x2 !== '' ? (
+        {images.x1 || images.x2 ? (
           <img
             loading="lazy"
             srcSet={`${images.x1} 1x, ${images.x2} 2x`}
             src={images.x1}
-            alt={images.alt}
+            alt={images.alt || 'Зображення'}
           />
         ) : (
           <div className={css.placeholderWrapper}>
             <svg className={css.imgPlaceholder}>
-              <use href={svg + '#placeholder'}></use>
+              <use href={`${svg}#placeholder`}></use>
             </svg>
           </div>
         )}
